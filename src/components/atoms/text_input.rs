a346f8012e79512cd77fe5af430c8141aac8cc68
TONY_LEGION <firesloyka@gmail.com>
2026-03-02 12:58:39 +0300

    20260302

--- src/components/atoms/text_input.rs
+++ src/components/atoms/text_input.rs
@@ -1,4 +1,9 @@
+use gloo::file::ObjectUrl;
+use serde::de::value;
+use wasm_bindgen::JsCast;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
+use gloo::console::log;
 
 #[derive(Properties, PartialEq)]
  
@@ -9,7 +14,17 @@ use yew::prelude::*;
 
 #[function_component(TextInput)]
 pub fn text_input(props: &Props) -> Html {
+    let onchange = Callback::from(|event: Event| {
+        let value = event
+        .target()
+        .unwrap()
+        .unchecked_into::<HtmlInputElement>()
+        .value();
+        log!(value);
+    
+
+    });
     html! {
-        <input type="text" name={props.name.clone()} />
+        <input type="text" name={props.name.clone()} onchange={onchange} />
     }
 }
